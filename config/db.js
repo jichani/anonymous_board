e8a7ada49199fc87fb3d383fe4ca249d1305fc85
Jichani <123962013+jichani@users.noreply.github.com>
2024-03-02 23:28:40 +0900

    클론 코딩은 완료. todo 남았음!

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
-module.export = {
+module.exports = {
   host: "127.0.0.1",
   user: "anonyWebAdm",
-  port: "3307",
+  port: "3306",
   password: "1234",
   database: "anony_web",
+  authPlugin: "mysql_native_password",
 };
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const dbConfig = require("./config/db.js");
 const connection = mysql.createConnection(dbConfig);
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs"), app.set("views", "./layout");
 app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 3003);
@@ -17,11 +19,55 @@ app.get("/", (req, res) => {
   }
   const cntPerPage = 9;
 
-  connection.query("SELECT count(*) as total FROM article where board_id 1", (error, rows) => {
+  connection.query("SELECT count(*) as total FROM article WHERE board_id = 1", (error, rows) => {
     if (error) throw error;
 
     let totalCnt = rows[0]["total"];
     let maxPage = Math.floor(totalCnt / cntPerPage) + (totalCnt % cntPerPage > 0 ? 1 : 0);
+
+    console.log("Page", page, "maxPage", maxPage, "total", totalCnt);
+
+    if (page < 0) {
+      res.redirect("./?page=1");
+    } else if (page > maxPage + 1) {
+      res.redirect("./?page=" + maxPage);
+    } else {
+      connection.query(
+        "SELECT * FROM article WHERE board_id = 1 ORDER BY article_id DESC limit ?,?",
+        [(page - 1) * cntPerPage, cntPerPage],
+        (error, rows) => {
+          if (error) throw error;
+
+          for (let i = 0; i < rows.length; i++) {
+            let date = new Date(rows[i]["reg_date"]);
+            const dateStr =
+              date.getFullYear() +
+              "." +
+              date.getMonth() +
+              "." +
+              date.getDate() +
+              " " +
+              date.getHours() +
+              ":" +
+              date.getMinutes();
+
+            datas.push({
+              id: rows[i]["article_id"],
+              date: dateStr,
+              title: rows[i]["title"],
+              content: rows[i]["content"],
+            });
+          }
+
+          res.render("board/listPage", {
+            datas: datas,
+            cnt: datas.length,
+            prevPage: page - 1 > 0 ? page - 1 : 1,
+            nextPage: page + 1 > maxPage ? maxPage : page + 1,
+          });
+        }
+      );
+    }
   });
 });
 
@@ -34,11 +80,63 @@ app.get("/class", (req, res) => {
 });
 
 app.get("/view", (req, res) => {
-  res.sendFile(__dirname + "/public/html/view.html");
+  let articleId = req.query.id;
+  connection.query("SELECT * FROM article WHERE board_id = 1 and article_id = ?", [articleId], (error, rows) => {
+    if (error) throw error;
+
+    let date = new Date(rows[0]["reg_date"]);
+    const dateStr =
+      date.getFullYear() +
+      "." +
+      date.getMonth() +
+      "." +
+      date.getDate() +
+      " " +
+      date.getHours() +
+      ":" +
+      date.getMinutes();
+
+    let boardViewData = {
+      title: rows[0]["title"],
+      content: rows[0]["content"],
+      date: dateStr,
+      id: rows[0]["article_id"],
+    };
+
+    res.render("board/viewpage", {
+      article: boardViewData,
+    });
+  });
 });
 
 app.get("/write", (req, res) => {
-  res.sendFile(__dirname + "/public/html/write.html");
+  res.render("board/writepage");
+});
+
+app.post("/write", (req, res) => {
+  connection.query("SELECT * FROM board where board_id = 1", (error, rows) => {
+    if (error) throw error;
+    const dbCryptKey = rows[0]["crypt_key"];
+
+    if (dbCryptKey == req.body.code) {
+      let title = req.body.title;
+      let content = req.body.content;
+
+      connection.query(
+        "INSERT INTO article (board_id, title, content, reg_date) VALUES (1,?,?,?)",
+        [title, content, new Date()],
+        (error, rows) => {
+          if (error) throw error;
+
+          res.redirect("/view?id=" + rows.insertId);
+        }
+      );
+    } else {
+      // todo 익명코드 틀렸을 때 알림 뜨게하기
+      // todo id 잘못 적었을 때 잘못된 페이지입니다. 알려주기
+      res.redirect("/write");
+    }
+  });
 });
 
 app.listen(app.get("port"), () => {
