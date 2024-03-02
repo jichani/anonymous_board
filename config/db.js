94b062636f6509bdac0b765b338c1fc8c0a283bc
Jichani <123962013+jichani@users.noreply.github.com>
2024-03-02 14:18:09 +0900

    db 연결 중

---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.export = {
+  host: "127.0.0.1",
+  user: "anonyWebAdm",
+  port: "3307",
+  password: "1234",
+  database: "anony_web",
+};
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mysql = require("mysql");
+const dbConfig = require("./config/db.js");
+const connection = mysql.createConnection(dbConfig);
+
 const app = express();
 
 app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 3003);
+
 app.get("/", (req, res) => {
-  res.end("Hello world");
+  let datas = [];
+  let page = 1;
+  if (req.query && req.query.page) {
+    page = req.query.page;
+  }
+  const cntPerPage = 9;
+
+  connection.query("SELECT count(*) as total FROM article where board_id 1", (error, rows) => {
+    if (error) throw error;
+
+    let totalCnt = rows[0]["total"];
+    let maxPage = Math.floor(totalCnt / cntPerPage) + (totalCnt % cntPerPage > 0 ? 1 : 0);
+  });
 });
 
 app.get("/index", (req, res) => {
